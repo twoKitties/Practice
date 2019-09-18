95d109a69be90534393e5c3d84a3bbb71997882c
Alex <nemyrexe@gmail.com>
2019-09-18 13:12:56 +0700

    init commit

---
+++ Practice_3/GarbageCollector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
